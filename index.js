38c48e88561db1f5516bb82e513e70f11485f4ac
Adeel <muhammadadeel7831@gmail.com>
2025-02-22 14:35:58 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function showMenu(){
+    if(document.getElementsByClassName('hamBurger')[0].style.display == 'none'){
+        document.getElementsByClassName('hamBurger')[0].style.display = 'block';
+    }else{
+        document.getElementsByClassName('hamBurger')[0].style.display = 'none';
+    }
+}
